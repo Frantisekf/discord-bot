039e2d0026b55ac6f96388267dd8bfe3bd0dbf49
Frantisek Farkas <frantisek@thefemalecompany.com>
2020-04-19 20:03:15 +0200

    added new module

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,11 @@
-import Command from '../command';
-import RandomNumber from './randomNumber'
-
+import Command from "../command";
+import RandomNumber from "./randomNumber";
+import DadJoke from "./dadjoke";
 
 const CommandsList: Record<string, typeof Command> = {
   // command names must be lowercase
   random: RandomNumber,
-
+  joke: DadJoke,
 };
 
 export default CommandsList;
