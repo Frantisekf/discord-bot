b6b3ca9661f3afdfd55aa94e88b56f3c3dc659d9
Lukas Chmelar <mail@lukaschmelar.sk>
2020-04-19 21:33:26 +0200

    RNG upgraded

--- src/commands/randomNumber.ts
+++ src/commands/randomNumber.ts
@@ -3,6 +3,6 @@ import { getRandomInt } from '../utils/random'
 
 export default class RandomNumber extends Command {
     async execute() {
-        this.message.reply(getRandomInt(-5, 180))
+        this.message.reply(getRandomInt(-5, 500))
     }
 }
