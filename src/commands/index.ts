8ceaab1c60a3da714ee7cef5e92ad26b27ca9ee0
Lukas Chmelar <mail@lukaschmelar.sk>
2020-04-20 18:34:42 +0200

    Text to speech command added

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,12 @@
 import Command from "../command";
 import RandomNumber from "./randomNumber";
 import DadJoke from "./dadjoke";
+import Say from "./say";
 
 const CommandsList: Record<string, typeof Command> = {
   randomNumber: RandomNumber,
   dadJoke: DadJoke,
+  say: Say,
 };
 
 export default CommandsList;
